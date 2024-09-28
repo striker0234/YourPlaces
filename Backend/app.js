fb599a860fa3dbd34fa6c7befc8ed9f256683329
Vinit Jain <vinitjnnn@gmail.com>
2024-09-28 18:42:10 +0530

    update

---
+++ Backend/app.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use(
+  cors({
+  origin: '*',
+  credentials: true,
+})
+)
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5j5oe7g.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(2000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
